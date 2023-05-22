16b11936f12cf86a278a3b8698125f48c39bf44c
Roberto Alencar <rroberto.alencar@gmail.com>
2023-05-22 15:44:39 -0300

    Conteúdo 14 e 15

--- src/views/cliente/ListCliente.jsx
+++ src/views/cliente/ListCliente.jsx
@@ -97,12 +97,14 @@ class ListCliente extends React.Component{
                                             <Table.Cell>{cliente.foneFixo}</Table.Cell>
                                             <Table.Cell textAlign='center'>
                                               
-                                                <Button
-                                                   inverted
-                                                   circular
-                                                   icon='edit'
-                                                   color='blue'
-                                                   itle='Clique aqui para editar os dados deste cliente' /> &nbsp;
+                                            <Button
+                                                inverted
+                                                circular
+                                                color='green'
+                                                title='Clique aqui para editar os dados deste cliente'
+                                                icon>
+                                                    <Link to="/form-cliente" state={{id: cliente.id}} style={{color: 'green'}}> <Icon name='edit' /> </Link>
+                                            </Button> &nbsp;
                                                    
                                                 <Button
                                                    inverted
