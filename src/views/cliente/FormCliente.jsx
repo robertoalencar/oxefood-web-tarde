62b98ae26f6828a193a000e8700c80b2d10477be
Roberto Alencar <rroberto.alencar@gmail.com>
2024-03-20 17:52:49 -0300

    atualizando projeto

--- src/views/cliente/FormCliente.jsx
+++ src/views/cliente/FormCliente.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import InputMask from 'react-input-mask';
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+import MenuSistema from '../../MenuSistema';
 
 export default function FormCliente () {
 
@@ -33,6 +34,8 @@ export default function FormCliente () {
     return (
 
         <div>
+
+            <MenuSistema tela={'cliente'} />
 
             <div style={{marginTop: '3%'}}>
 
