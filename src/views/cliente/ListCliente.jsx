b0256092818f34cc6183b9b43ede8339e24dea69
Roberto Alencar <rroberto.alencar@gmail.com>
2024-07-04 15:59:49 -0300

    Enviado arquivos da aula

--- src/views/cliente/ListCliente.jsx
+++ src/views/cliente/ListCliente.jsx
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
+import { Button, Container, Divider, Header, Icon, Modal, Table } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
 export default function ListCliente () {
 
     const [lista, setLista] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+    const [idRemover, setIdRemover] = useState();
 
     useEffect(() => {
 
@@ -30,6 +32,31 @@ export default function ListCliente () {
 
         let arrayData = dataParam.split('-');
         return arrayData[2] + '/' + arrayData[1] + '/' + arrayData[0];
+    }
+
+    function confirmaRemover(id) {
+
+        setOpenModal(true)
+        setIdRemover(id)
+    }
+
+    async function remover() {
+
+        await axios.delete('http://localhost:8080/api/cliente/' + idRemover)
+        .then((response) => {
+  
+            console.log('Cliente removido com sucesso.')
+  
+            axios.get("http://localhost:8080/api/cliente")
+            .then((response) => {
+                setLista(response.data)
+            })
+        })
+        .catch((error) => {
+            console.log('Erro ao remover um cliente.')
+        })
+        
+        setOpenModal(false)
     }
 
     return(
@@ -90,16 +117,17 @@ export default function ListCliente () {
                                                 color='green'
                                                 title='Clique aqui para editar os dados deste cliente'
                                                 icon>
-                                                    <Icon name='edit' />
+                                                    <Link to="/form-cliente" state={{id: cliente.id}} style={{color: 'green'}}> <Icon name='edit' /> </Link>
                                             </Button> &nbsp;
                                             <Button
-                                               inverted
-                                               circular
-                                               color='red'
-                                               title='Clique aqui para remover este cliente'
-                                               icon>
+                                                inverted
+                                                circular
+                                                color='red'
+                                                title='Clique aqui para remover este cliente'
+                                                icon
+                                                onClick={e => confirmaRemover(cliente.id)}>
                                                    <Icon name='trash' />
-                                           </Button>
+                                            </Button>
 
                                         </Table.Cell>
                                     </Table.Row>
@@ -110,6 +138,27 @@ export default function ListCliente () {
                     </div>
                 </Container>
             </div>
+
+            <Modal
+                basic
+                onClose={() => setOpenModal(false)}
+                onOpen={() => setOpenModal(true)}
+                open={openModal}
+            >
+                <Header icon>
+                    <Icon name='trash' />
+                    <div style={{marginTop: '5%'}}> Tem certeza que deseja remover esse registro? </div>
+                </Header>
+                <Modal.Actions>
+                    <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
+                        <Icon name='remove' /> Não
+                    </Button>
+                    <Button color='green' inverted onClick={() => remover()}>
+                        <Icon name='checkmark' /> Sim
+                    </Button>
+                </Modal.Actions>
+            </Modal>
+
         </div>
     )
 }
